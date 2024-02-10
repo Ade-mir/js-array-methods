f3e18b665e20c6990e86c5f66c710d95cd2f981c
Ademir <115159286+Ade-mir@users.noreply.github.com>
2024-02-10 18:11:20 +0100

    Add (at) and (concat) method

---
+++ at/script.js
@@ -0,0 +1,17 @@
+// const myArray = [5, 12, 8, 130, 44];
+
+// const returnedIndex = myArray.at(-2);
+
+// console.log(returnedIndex);
+
+// const myArray = ['milk', 'apple', 'egg'];
+
+// const returnedIndex = myArray.at(0);
+
+// console.log(returnedIndex);
+
+const myArray = ['carrot', true, 23, null];
+
+const returnedIndex = myArray.at(-3);
+
+console.log(returnedIndex);
---
+++ concat/script.js
@@ -0,0 +1,14 @@
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+
+// const array3 = array1.concat(array2);
+
+// console.log(array3);
+
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = ['d', 'e', 'f'];
+
+const array4 = array1.concat(array2, array3);
+
+console.log(array4);
